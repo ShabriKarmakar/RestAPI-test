f861134dd38c0c9a25331ed9394a2c950f4e5802
ShabriKarmakar <sabari.karmakar@gmail.com>
2025-04-27 19:15:24 +0530

    save List

--- src/main/java/org/example/Controller.java
+++ src/main/java/org/example/Controller.java
@@ -2,6 +2,8 @@ package org.example;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/")
 public class Controller {
@@ -16,5 +18,11 @@ public class Controller {
     public Object saveCall(@RequestBody Object testObject) {
         System.out.println("Test post call");
         return testObject;
+    }
+
+    @PostMapping("/saveList")
+    public List<Object> saveList(@RequestBody List<Object> objectList) {
+        System.out.println("Test post call");
+        return objectList;
     }
 }
