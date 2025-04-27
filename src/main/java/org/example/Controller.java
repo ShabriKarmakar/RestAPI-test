abd012e33a21bc1c888ee9e8b95762a5fb33ac27
ShabriKarmakar <sabari.karmakar@gmail.com>
2025-04-27 19:33:25 +0530

    sout change

--- src/main/java/org/example/Controller.java
+++ src/main/java/org/example/Controller.java
@@ -22,7 +22,7 @@ public class Controller {
 
     @PostMapping("/saveList")
     public List<Object> saveList(@RequestBody List<Object> objectList) {
-        System.out.println("Test post call");
+        System.out.println("Test post call" + objectList.toString());
         return objectList;
     }
 }
